760d1b7eced199e380a082557a29c8bd770518b3
kurokuro96 <40589691+kurokuro96@users.noreply.github.com>
2022-04-29 11:29:57 +0000

    Update test to output generated at c07add9

---
+++ _nuxt/static/1651231792/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Frecord","\u002Fabout","\u002Fworks","\u002Fworks\u002Fprogram","\u002Fworks\u002Fmagazine","\u002Fworks\u002Fwebsite","\u002Factivity","\u002Fagreements"]})
