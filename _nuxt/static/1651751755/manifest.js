c9daedbb75bf8a3f91855da49b12e99fe8cb462d
DualXeon51 <72848557+DualXeon51@users.noreply.github.com>
2022-05-05 11:56:02 +0000

    Update public to output generated at b0dd4cf

---
+++ _nuxt/static/1651751755/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fabout","\u002Frecord","\u002Fworks","\u002Factivity","\u002Fagreements","\u002Fworks\u002Fprogram","\u002Fworks\u002Fwebsite","\u002Fworks\u002Fmagazine"]})
